201d8e51f3019a869770d473acb022bf15c2d964
Uy <uy.huynh.cit24@eiu.edu.vn>
2026-04-06 12:48:39 +0700

    this is a comment changes

--- test.java
+++ test.java
@@ -5,7 +5,7 @@ public class test {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int number = scanner.nextInt();
-
+        //Test changes
         if (number % 2 == 0) {
             System.out.println(number + " is even.");
         } else {
